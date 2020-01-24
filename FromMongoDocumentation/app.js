859bf92b2da25b6f76c35e134a99fe35dcef38b5
Victor-Chinewubeze <victor.chinewubeze@gmail.com>
2020-01-24 11:59:07 +0100

    Add code for Derek Banas tutorial

--- FromMongoDocumentation/app.js
+++ FromMongoDocumentation/app.js
@@ -22,7 +22,7 @@ MongoClient.connect(url, function(err, client) {
 
 
 console.log("Please play around with the available functions\nJust edit or add to the ones below")
-indexCollection(db, function() {
+updateDocument(db, function() {
     findDocuments(db, function(){
         client.close();
         console.log("Try your hands on various functions and edit the ones that were called above from the above ")
@@ -63,8 +63,8 @@ const insertDocuments = function(db, callback) {
     // Get the documents collection
     const collection = db.collection('documents');
     // Update document where a is 2, set b equal to 1
-    collection.updateOne({ a : 2 }
-      , { $set: { b : 5 } }, function(err, result) {
+    collection.updateOne({ vee : 23 }
+      , { $set: { b : 5, a : 14, c: 15 } }, function(err, result) {
       assert.equal(err, null);
       assert.equal(1, result.result.n);
       console.log("Updated the document with the field a equal to 2");
